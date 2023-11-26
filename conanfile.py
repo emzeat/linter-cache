3917c61a156851e5ca56c3238740fda81700807a
Marius Zwicker <marius.zwicker@mlba-team.de>
2023-11-26 11:00:11 +0100

    Update conan recipe
    Switch to modern cmake integration

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,9 @@
 
 import os
 
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
+from conan.tools.files import copy
 
 
 class LinterCacheConan(ConanFile):
@@ -23,8 +25,6 @@ class LinterCacheConan(ConanFile):
     license = "Apache 2.0"
     description = "Wrapper to invoke code linters through ccache to accelerate analysis as part of a regular compile job."
     url = "https://emzeat.de/linter-cache"
-
-    generators = "cmake_find_package", "json"
 
     settings = "os", "compiler", "build_type", "arch"
 
@@ -38,23 +38,26 @@ class LinterCacheConan(ConanFile):
     def export_sources(self):
         self.copy("*", excludes=["build/*-*-*"])
 
-    def requirements(self):
-        pass
-
     def build_requirements(self):
         self.tool_requires("ccache/4.6")
         self.tool_requires("clang-tools-extra/13.0.1@emzeat/external")
         if self.options.build_tests:
-            self.test_requires("gtest/1.11.0")
+            self.test_requires("gtest/1.14.0")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        tc = CMakeToolchain(self, generator='Ninja')
+        tc.cache_variables["MZ_DO_AUTO_FORMAT"] = False
+        tc.cache_variables["MZ_DO_CPPLINT"] = False
+        tc.cache_variables["MZ_DO_CPPLINT_DIFF"] = False
+        tc.cache_variables['BUILD_LINTER_CACHE_TESTS'] = self.options.build_tests
+        tc.generate()
 
     def _configure_cmake(self):
-        cmake = CMake(self, generator='Ninja')
-        cmake.definitions["MZ_DO_AUTO_FORMAT"] = False
-        cmake.definitions["MZ_DO_CPPLINT"] = False
-        cmake.definitions["MZ_DO_CPPLINT_DIFF"] = False
-        cmake.definitions['BUILD_LINTER_CACHE_TESTS'] = self.options.build_tests
-        cmake.configure(source_folder=self.source_folder,
-                        build_folder=self.build_folder)
+        cmake = CMake(self)
+        cmake.configure()
         return cmake
 
     def build(self):
@@ -69,11 +72,7 @@ class LinterCacheConan(ConanFile):
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
-        self.copy("LICENSE", src=self.source_folder, dst="licenses")
+        copy(self, "LICENSE", src=self.source_folder, dst="licenses")
 
     def package_info(self):
         self.cpp_info.bindirs = ['bin']
-
-        bin_path = os.path.join(self.package_folder, "bin")
-        self.output.info(f"Appending PATH environment variable: {bin_path}")
-        self.env_info.path.append(bin_path)
